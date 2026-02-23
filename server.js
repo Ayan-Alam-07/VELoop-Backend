b1c5f192fff91887f3927c4120b831e966be16f3
Ayan-Alam-07 <infosys316@gmail.com>
2026-02-23 17:37:00 +0530

    server code updated

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected"));
+
+// app.use("/api/auth", require("./routes/auth"));
+
+// app.listen(5000, () => console.log("Server running on 5000"));
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -10,8 +28,11 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
