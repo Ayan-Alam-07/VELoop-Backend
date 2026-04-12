465e3f2712ff471afd25548f3f633aaa83a00179
Ayan-Alam-07 <infosys316@gmail.com>
2026-04-12 18:54:47 +0530

    updating coins to VEs

--- config/seedDailyCheckinRewards.js
+++ config/seedDailyCheckinRewards.js
@@ -14,37 +14,37 @@ const seedDailyCheckinRewards = async () => {
     const rewards = [
       {
         day: 1,
-        reward: "5 Coins",
+        reward: "5 VEs",
         type: "coin",
         value: "+5",
       },
       {
         day: 2,
-        reward: "10 Coins",
+        reward: "10 VEs",
         type: "coin",
         value: "+10",
       },
       {
         day: 3,
-        reward: "20 Coins",
+        reward: "20 VEs",
         type: "coin",
         value: "+20",
       },
       {
         day: 4,
+        reward: "₹1 Amazon Gift Card",
+        type: "giftcard",
+        value: "₹1",
+      },
+      {
+        day: 5,
         reward: "₹2 Amazon Gift Card",
         type: "giftcard",
         value: "₹2",
       },
       {
-        day: 5,
-        reward: "₹3 Amazon Gift Card",
-        type: "giftcard",
-        value: "₹3",
-      },
-      {
         day: 6,
-        reward: "40 Coins",
+        reward: "40 VEs",
         type: "coin",
         value: "+40",
       },
