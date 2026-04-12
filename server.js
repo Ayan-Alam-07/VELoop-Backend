49a48ded847b6b412411d721912b88d6370b4572
Ayan-Alam-07 <infosys316@gmail.com>
2026-04-12 15:34:17 +0530

    updating the server routes

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
 
 const transactionRoutes = require("./routes/transaction");
 const exchangeRoutes = require("./routes/exchange");
