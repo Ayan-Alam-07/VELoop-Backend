72dd5e576d053009439c5f04f01c4355552c674e
Ayan-Alam-07 <infosys316@gmail.com>
2026-04-23 21:36:22 +0530

    updating the code to fix the error

--- utils/weightedRandom.js
+++ utils/weightedRandom.js
@@ -1,20 +1,48 @@
-const weightedRandom = (rewards) => {
-  const totalWeight = rewards.reduce(
-    (sum, reward) => sum + reward.probability,
+// const weightedRandom = (rewards) => {
+//   const totalWeight = rewards.reduce(
+//     (sum, reward) => sum + reward.probability,
+//     0,
+//   );
+
+//   let random = Math.random() * totalWeight;
+
+//   for (const reward of rewards) {
+//     random -= reward.probability;
+
+//     if (random <= 0) {
+//       return reward;
+//     }
+//   }
+
+//   return rewards[0];
+// };
+
+// module.exports = weightedRandom;
+
+const weightedRandom = (items) => {
+  if (!items || items.length === 0) return null;
+
+  const validItems = items.filter(
+    (item) => typeof item.probability === "number" && item.probability > 0,
+  );
+
+  if (!validItems.length) return items[0];
+
+  const totalWeight = validItems.reduce(
+    (sum, item) => sum + item.probability,
     0,
   );
 
   let random = Math.random() * totalWeight;
 
-  for (const reward of rewards) {
-    random -= reward.probability;
-
-    if (random <= 0) {
-      return reward;
+  for (const item of validItems) {
+    if (random < item.probability) {
+      return item;
     }
+    random -= item.probability;
   }
 
-  return rewards[0];
+  return validItems[0];
 };
 
 module.exports = weightedRandom;
