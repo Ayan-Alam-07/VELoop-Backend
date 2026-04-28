df648b28cd9fdb4f5234ebf4984755702fd5b55f
Ayan-Alam-07 <infosys316@gmail.com>
2026-04-28 21:26:20 +0530

    fixing 5oo error

--- controllers/levelController.js
+++ controllers/levelController.js
@@ -1,23 +1,57 @@
+// const User = require("../models/User");
+// const { calculateLevel } = require("../utils/levelUtils");
+// const { badges } = require("../config/badgeConfig");
+// const { LEVELS } = require("../config/levelConfig");
+
+// const getMyLevel = async (req, res) => {
+//   const user = await User.findById(req.user.id);
+//   const lvl = LEVELS.find((l) => l.level === user.level);
+
+//   const { nextXP } = calculateLevel(user.xp);
+
+//   res.json({
+//     lvlReward: lvl.reward,
+//     level: user.level,
+//     xp: user.xp,
+//     coins: user.coins,
+//     nextXP,
+//     badge: badges[user.level],
+//     progress: (user.xp / nextXP) * 100,
+//   });
+// };
+
+// module.exports = { getMyLevel };
+
 const User = require("../models/User");
 const { calculateLevel } = require("../utils/levelUtils");
 const { badges } = require("../config/badgeConfig");
 const { LEVELS } = require("../config/levelConfig");
 
 const getMyLevel = async (req, res) => {
-  const user = await User.findById(req.user.id);
-  const lvl = LEVELS.find((l) => l.level === level);
+  try {
+    const user = await User.findById(req.user.id);
 
-  const { nextXP } = calculateLevel(user.xp);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-  res.json({
-    lvlReward: lvl.reward,
-    level: user.level,
-    xp: user.xp,
-    coins: user.coins,
-    nextXP,
-    badge: badges[user.level],
-    progress: (user.xp / nextXP) * 100,
-  });
+    const lvl = LEVELS.find((l) => l.level === user.level);
+
+    const { nextXP } = calculateLevel(user.xp);
+
+    res.json({
+      lvlReward: lvl?.reward || 0,
+      level: user.level,
+      xp: user.xp,
+      coins: user.coins,
+      nextXP,
+      badge: badges[user.level],
+      progress: (user.xp / nextXP) * 100,
+    });
+  } catch (error) {
+    console.error("LEVEL ERROR:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 module.exports = { getMyLevel };
